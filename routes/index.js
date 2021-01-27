d5b6e03b4958424daf533c8f3d3eabacee446a0d
Zutar <Leon9539@gmail.com>
2021-01-27 10:55:16 +0200

    fix

--- routes/index.js
+++ routes/index.js
@@ -53,7 +53,7 @@ module.exports = (function(clickhouse){
     });
 
     router.get('/getChart', (req, res) => {
-        const query = `SELECT * FROM stream_data ORDER BY DESC LIMIT 10000;`;
+        const query = `SELECT * FROM stream_data ORDER BY timestamp DESC LIMIT 10000;`;
         clickhouse.query(query).exec(function (err, rows) {
             res.send(rows);
         });
